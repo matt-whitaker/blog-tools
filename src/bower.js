d99458c2292d134557046cf4ec623f1a7a198c49
Matt Whitaker <matt.whitaker@concur.com>
2018-04-01 11:27:07 -0700

    test

--- src/bower.js
+++ src/bower.js
@@ -5,8 +5,19 @@ import printFiles from './utils/printFiles';
 export default function(config) {
   const { dstRoot } = config.get('build');
 
+  console.log(process.cwd());
+  console.log(__dirname);
+  console.log(__filename);
+
+
   return () =>
-    gulp.src(bower(), { base: 'bower_components' })
+    gulp.src(bower({
+      options: {
+        paths: {
+          bowerJson: 'node_modules/blog-tools/bower.json'
+        }
+      }
+    }), { base: 'node_modules/blog-tools/bower_components' })
       .pipe(gulp.dest(dstRoot))
       .pipe(printFiles('bower'));
 }

1aca94cfd7fc4ca6a1bce32c6312b184255b65cc
Matt Whitaker <matt.whitaker@concur.com>
2018-04-01 12:44:46 -0700

    trying this

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
-  NODE_ENV=production npm run build
+  NODE_ENV=production npx gulp build
 else
-  NODE_ENV=qa npm run build
+  NODE_ENV=qa npx gulp build
 fi
 
 echo "Built site on $CIRCLE_BRANCH"
